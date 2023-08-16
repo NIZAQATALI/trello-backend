85c0ec5b054a82986008c968fd388588809584c0
Nizaqat Ali <hafiznizaqatali@gmail.com>
2023-08-16 13:14:26 +0500

    Updated code with cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose= require('mongoose');
 const dotenv=require('dotenv')
+const cors = require('cors');
 const express = require('express');
 dotenv.config({path:'./config.env'});
 const app = express()
@@ -15,6 +16,8 @@ mongoose.connect(process.env.MONGO_URL, {
   const userRoute = require("./routes/userRoute");
   const boardRoute = require("./routes/boardRoute");
  app.use(express.json())
+
+ app.use(cors());
  //   app.use("/api/auth", authRoute);
   //  app.use("/api/users", userRoute);
     app.use('/user', userRoute);
